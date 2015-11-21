0864a9ef31e1feef2eb6f9ab116b7309cac0ade8
sergeyz <sergzak022@gmail.com>
2015-11-21 13:37:01 -0800

    added some changes to ui, also switched to jquery 1.7.0

--- client/lib/apps/main-layout/apps/welcome/apps/login/Model.js
+++ client/lib/apps/main-layout/apps/welcome/apps/login/Model.js
@@ -1,12 +1,15 @@
 define(['Backbone', 'jquery'], function (Backbone, $) {
     return Backbone.Model.extend({
         defaults: {
-
+            email: null,
+            name: null,
+            password: null,
+            antPlanerId: null
         },
 
         initialize: function () {
 
-        },
+        }
 
 
     });
--- client/lib/apps/main-layout/apps/welcome/apps/login/View.js
+++ client/lib/apps/main-layout/apps/welcome/apps/login/View.js
@@ -2,15 +2,30 @@
 
 define(['Backbone', 'underscore', 'css!./stylesheets/style'], function (Backbone, _) {
     return Backbone.View.extend({
-        template: _.template("\
-            <div> \
-            </div> \
-        "),
+        template: _.template('\
+            <div id="login" class="wrapper">\
+                <form class="form-register">\
+                  <h2 class="form-register-heading">Enter your information</h2>\
+                  <input type="text" class="form-control" name="name" placeholder="Name" required="" autofocus="" />\
+                  <input type="text" class="form-control" name="email" placeholder="Email Address" required="" autofocus="" />\
+                  <input type="text" class="form-control" name="ant-planer-id" placeholder="AntPlaner ID" required=""/>\
+                  <input type="password" class="form-control" name="password" placeholder="Password" required=""/>\
+                  <input type="password" class="form-control" name="confirm-password" placeholder="Confirm Password" required=""/>\
+                  <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>\
+                </form>\
+            </div>\
+        '),
 
-        _views: {},
+        events: {
+            'click button[type="submit"]' : 'register'
+        },
 
         initialize: function () {
 
+        },
+
+        register : function () {
+            console.log('registering')
         },
 
         remove: function (empty) {
@@ -25,7 +40,7 @@ define(['Backbone', 'underscore', 'css!./stylesheets/style'], function (Backbone
         },
 
         render: function () {
-
+            this.$el.append(this.template());
             return this;
         }
     });
