c1601c37298294bc6ec37fac1eaec878dd9b2104
sergeyz <sergzak022@gmail.com>
2015-11-21 18:55:11 -0800

    updated group-ui

---
+++ client/lib/apps/main-layout/View.js
@@ -0,0 +1,72 @@
+/*jshint multistr: true, eqnull:true */
+
+define(['Backbone', 'underscore', 'login', 'register', 'css!./stylesheets/style'], function (Backbone, _, login, register) {
+    return Backbone.View.extend({
+        template: _.template('\
+            <div id="welcome" class="wrapper">\
+                <div class="switch-wiew">\
+                    <ul class="nav nav-tabs nav-justified">\
+                        <li><a class="login" href="#">Login</a></li>\
+                        <li><a class="register" href="#">Register</a></li>\
+                    </ul>\
+                </div>\
+                <form class="form-login-register">\
+                </form>\
+            </div>\
+        '),
+
+        events: {
+            'click a.login' : 'renderLoginForm',
+            'click a.register' : 'renderRegisterForm'
+        },
+
+        _curPanel: null,
+
+        initialize: function () {
+            this._loginController = login;
+            this._registerController = register;
+        },
+
+        renderLoginForm: function () {
+            if ( this._curPanel !== 'login') {
+                this._registerController.destroyPanel(true);
+                this._loginController.createPanel(this.$el.find('.form-login-register'));
+                this._curPanel = 'login';
+            }
+        },
+
+        renderRegisterForm: function () {
+            if ( this._curPanel !== 'register') {
+                this._loginController.destroyPanel(true);
+                this._registerController.createPanel(this.$el.find('.form-login-register'));
+                this._curPanel = 'register';
+            }
+        },
+
+        register : function (e) {
+            e.preventDefault();
+            var values = {};
+            values.email = this.$el.find("input[name='email']").val();
+            values.password = this.$el.find("input[name='password']").val();
+
+            this.model.register(values);
+        },
+
+        remove: function (empty) {
+            if (empty) {
+                this.trigger('remove');
+                this.$el.empty().off();
+                this.stopListening();
+                return this;
+            } else {
+                return Backbone.View.prototype.remove.call(this);
+            }
+        },
+
+        render: function () {
+            this.$el.append(this.template());
+            this.renderLoginForm();
+            return this;
+        }
+    });
+});
--- client/lib/apps/main-layout/apps/group-list/GroupRow.js
+++ client/lib/apps/main-layout/apps/group-list/GroupRow.js
@@ -3,7 +3,30 @@ define(['Backbone', 'jquery'], function (Backbone, $) {
         defaults: {
             name: null, //group name
             owner: null, //group owner
+            id: null,
             members: []
+        },
+
+        sendCalenderRerenderRequest: function () {
+            var emails = this.members.map(function (member) {
+                return member.email;
+            });
+
+            $.post('/schedules', {
+                email: emails
+            }, function (res) {
+                console.log('Sent schedules request adn got response from server', res);
+                console.log('send rerender request')
+            });
+
+        },
+
+        addMember: function (email) {
+            $.post('/group/' + this.get('id') + '/members', {
+                email: email
+            }, function (res) {
+                console.log('Send add member request and got response from server', res);
+            });
         }
     });
 });
