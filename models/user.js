ac2d37493bd9a001002569008a30ca881c444066
Ubuntu <antgroup@antgroup.antgroup.g2.internal.cloudapp.net>
2015-11-22 09:09:02 +0000

    explicit foreign keys

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        User.belongsToMany(models.Group, { through: models.Membership })
+        User.belongsToMany(models.Group, { through: models.Membership, foreignKey: 'userId' })
       },
       login: function(email, password) {
         return User.findOne({ where: { email: email } })
